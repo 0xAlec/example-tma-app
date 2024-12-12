66862970860b9e5008bdbba6f5909d927edc4e79
Alec Chen <fkalec@gmail.com>
2024-12-12 15:41:42 -0800

    Update Button.tsx

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,8 +1,18 @@
 
 import WebApp from '@twa-dev/sdk'
+import { MainButton } from '@twa-dev/sdk/react';
+
+
+const biometricManager = WebApp.BiometricManager
+
+biometricManager.init()
 
 export default function AlertButton() {
-  return <button onClick={() => WebApp.showAlert(`Hello World!`)}>
-    Show Alert
-  </button>
+  return <MainButton text="Connect Wallet" onClick={() => biometricManager.authenticate({'reason': 'Authenticate to continue'}, (isAuthenticated) => {
+    if (isAuthenticated) {
+      WebApp.showAlert('Authenticated')
+    } else {
+      WebApp.showAlert('Not Authenticated')
+    }
+  })} />
 }
