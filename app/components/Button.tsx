d673f81d4db65b800874fd335e9931da2fe8faff
Alec Chen <fkalec@gmail.com>
2024-12-12 16:42:29 -0800

    Update Button.tsx

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -5,6 +5,6 @@ WebApp.ready()
 
 export default function AlertButton() {
   return <MainButton text="Connect Wallet" onClick={() => {
-    WebApp.openLink(`${process.env.VERCEL_URL}/auth`)
+    WebApp.openLink(`/auth`)
   }} />
 }
