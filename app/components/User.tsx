7778163251ac75e22199f277535191c6b32a3936
Alec Chen <fkalec@gmail.com>
2024-12-16 10:52:39 -0800

    mobile friendly

--- app/components/User.tsx
+++ app/components/User.tsx
@@ -3,16 +3,18 @@ import { useMiniContext } from '@lib/shared/MiniProvider';
 export default function User() {
     const { user, platform } = useMiniContext();
     
-    return <div>
-        <pre>
-            {JSON.stringify(
-                {
-                    user: user || null,
-                    platform: platform
-                },
-                null,
-                2
-            )}
-        </pre>
-    </div>
+    return (
+        <div className="w-full max-w-screen-lg mx-auto p-4 sm:p-6">
+            <pre className="bg-gray-100 rounded-lg p-4 overflow-x-auto text-sm sm:text-base whitespace-pre-wrap break-words">
+                {JSON.stringify(
+                    {
+                        user: user || null,
+                        platform: platform
+                    },
+                    null,
+                    2
+                )}
+            </pre>
+        </div>
+    );
 }
