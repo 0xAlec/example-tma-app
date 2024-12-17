2fca7a3040d97ed40139a5bfa69124fb0c938f39
Alec Chen <fkalec@gmail.com>
2024-12-17 15:10:08 -0800

    fix

--- app/components/Redirect.tsx
+++ app/components/Redirect.tsx
@@ -4,5 +4,8 @@ import { useMiniContext } from "@/lib/providers/MiniProvider";
 export default function Redirect() {
     const { openLink } = useMiniContext();
 
-    return <button onClick={() => openLink('/auth')}>Auth</button>;
+    return <button onClick={() => {
+        console.log(window.location.origin);
+        openLink(`${window.location.origin}/auth`)
+    }}>Auth</button>;
 }
