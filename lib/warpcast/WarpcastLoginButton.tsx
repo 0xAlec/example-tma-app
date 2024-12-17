3650e0a3a4e5d64267cf7087aa4bd81241263861
Alec Chen <fkalec@gmail.com>
2024-12-17 12:10:35 -0800

    disconnect

--- lib/warpcast/WarpcastLoginButton.tsx
+++ lib/warpcast/WarpcastLoginButton.tsx
@@ -1,13 +1,25 @@
-import { useAccount, useConnect } from 'wagmi'
+import { useAccount, useConnect, useDisconnect } from 'wagmi'
 
 export default function WarpcastLoginButton() {
   const { address, isConnected } = useAccount();
+  const { disconnect } = useDisconnect();
   const { connect, connectors } = useConnect()
 
   const connector = connectors[0];
 
   if (isConnected && address) {
-    return <div>Connected: {address}</div>
+    return (
+      <>
+        <div>Connected: {address}</div>
+        <br />
+        <button
+          className="bg-blue-600 border border-blue-700 text-white px-4 py-2 rounded hover:bg-blue-700 cursor-pointer"
+          onClick={() => disconnect()}
+        >
+          Disconnect
+        </button>
+      </>
+    )
   }
 
   return (
