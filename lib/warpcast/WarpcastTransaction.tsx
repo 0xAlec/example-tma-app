0ce5acf858b2b6016af8ffe3027b61fb16cb55cf
Alec Chen <fkalec@gmail.com>
2024-12-17 12:18:21 -0800

    UI

--- lib/warpcast/WarpcastTransaction.tsx
+++ lib/warpcast/WarpcastTransaction.tsx
@@ -5,11 +5,21 @@ import { useAccount } from "wagmi";
 
 export default function WarpcastTransaction() {
     const { address } = useAccount();
-    const { sendTransaction } = useSendTransaction();
+    const { sendTransaction, data, status } = useSendTransaction();
 
-    return <button onClick={() => sendTransaction({
-        to: address,
-        value: BigInt(100000000),
+    if (status === 'pending') {
+        return <div>Transaction pending...</div>
+    }
+
+    if (status === 'success') {
+        return <div>Transaction hash: {data}</div>
+    }
+
+    return <button
+        className="bg-blue-600 border border-blue-700 text-white px-4 py-2 rounded hover:bg-blue-700 cursor-pointer"
+        onClick={() => sendTransaction({
+            to: address,
+            value: BigInt(100000000),
         })} >
             Transaction
     </button>;
