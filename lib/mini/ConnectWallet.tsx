1abca2e776c21f79758f7c58ed9b11f72a686409
Alec Chen <fkalec@gmail.com>
2024-12-18 13:00:24 -0800

    Update ConnectWallet.tsx

--- lib/mini/ConnectWallet.tsx
+++ lib/mini/ConnectWallet.tsx
@@ -1,15 +1,22 @@
 import { useMiniContext } from "@/lib/providers/MiniProvider";
 import TelegramLoginButton from "../telegram/TelegramLoginButton";
 import WarpcastLoginButton from "../warpcast/WarpcastLoginButton";
+import { useDisconnect } from '@reown/appkit/react'
 
 export default function ConnectWallet() {
     const { platform } = useMiniContext();
+    const { disconnect } = useDisconnect();
 
     if (platform === 'warpcast') {
         return <WarpcastLoginButton />
     }
     if (platform === 'telegram') {
-        return <appkit-connect-button />
+        return (
+            <>
+                <appkit-button />
+                <button onClick={() => disconnect()}>Disconnect</button>
+            </>
+        )
     }
 
     return <TelegramLoginButton />
