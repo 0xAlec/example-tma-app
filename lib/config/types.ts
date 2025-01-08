7a33e22daf8d9d62553fc5b2262e607d4b88a668
Alec Chen <fkalec@gmail.com>
2025-01-08 13:09:41 -0500

    fix types

--- lib/config/types.ts
+++ lib/config/types.ts
@@ -1,3 +1,4 @@
 import type { User as TelegramUser } from '@telegram-apps/sdk';
+import type { Context } from '@farcaster/frame-sdk';
 
-export type MiniUser = TelegramUser;
+export type MiniUser = TelegramUser | Context.FrameContext;
